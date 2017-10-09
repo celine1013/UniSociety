042d16a719a9c096c3f67c8bca2b2d54fd18a159
celine1013 <celineliu1010@gmail.com>
2017-10-09 15:17:55 +1100

    buggy search by date

--- app/src/main/java/com/example/celine/unisociety/Fragment_HomePage.java
+++ app/src/main/java/com/example/celine/unisociety/Fragment_HomePage.java
@@ -84,30 +84,4 @@ public class Fragment_HomePage extends Fragment {
         recentEvent.setAdapter(adapter);
         return v;
     }
-
-    private static class PostViewHolder extends RecyclerView.ViewHolder {
-        public View postView;
-        public TextView tv_EventTitle;
-        public TextView tv_EventDate;
-        public TextView tv_EventTime;
-        public TextView tv_EventLocation;
-
-        public PostViewHolder(View itemView) {
-            super(itemView);
-            postView = itemView;
-            tv_EventTitle = (TextView) itemView.findViewById(R.id.tv_postTitle);
-            tv_EventDate = (TextView) itemView.findViewById(R.id.tv_eventDate);
-            tv_EventTime = (TextView) itemView.findViewById(R.id.tv_eventTime);
-            tv_EventLocation = (TextView) itemView.findViewById(R.id.tv_eventLocation);
-        }
-
-        public void setPost(Post post){
-            tv_EventTitle.setText(post.getPostTitle());
-            tv_EventDate.setText(post.getPostDate());
-            tv_EventTime.setText(post.getBeginTime()+"~"+post.getEndTime());
-            tv_EventLocation.setText(post.getLocation());
-        }
-
-    }
-
 }
