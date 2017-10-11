4c59fff84972a49dd57b30adaf3dc869453eb9e2
Fan <daniel25@126.com>
2017-10-11 16:03:43 +1100

    Image uploading

--- app/src/main/java/com/example/celine/unisociety/CreatePost_Activity.java
+++ app/src/main/java/com/example/celine/unisociety/CreatePost_Activity.java
@@ -3,6 +3,8 @@ package com.example.celine.unisociety;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,24 +14,36 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.Calendar;
 
 import Model.Post;
 
+import static android.R.attr.data;
+
 public class CreatePost_Activity extends AppCompatActivity {
     public static final String USER_ID = "user_id";
     private int societyID;
     private String postType;
+    private Post newPost;
 
     private EditText et_eventTitle;
     private EditText et_eventDes;
@@ -38,8 +52,14 @@ public class CreatePost_Activity extends AppCompatActivity {
     private Button bt_startingTime;
     private Button bt_endingTime;
     private EditText et_eventLocation;
+    private ImageView bt_selectPicture;
+
+    private static final int GALLERY_INTENT = 1;
 
     private Button bt_submit;
+
+    private Uri mImageUri;
+    private StorageReference mStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +71,7 @@ public class CreatePost_Activity extends AppCompatActivity {
             finish();
         }
 
+        mStorage = FirebaseStorage.getInstance().getReference();
 
         //binding
         et_eventTitle = (EditText) findViewById(R.id.title);
@@ -60,7 +81,9 @@ public class CreatePost_Activity extends AppCompatActivity {
         bt_startingTime = (Button) findViewById(R.id.bt_startTime);
         bt_endingTime = (Button) findViewById(R.id.bt_endTime);
         et_eventLocation = (EditText) findViewById(R.id.location);
-        bt_submit = (Button)findViewById(R.id.btn_Submit);
+        bt_selectPicture = findViewById(R.id.btn_selectPicture);
+        bt_submit = (Button) findViewById(R.id.btn_Submit);
+
 
         //set category spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -69,12 +92,19 @@ public class CreatePost_Activity extends AppCompatActivity {
         sp_eventCategory.setAdapter(adapter);
         //set time buttons
         setEventTimeBTS();
+        newPost = new Post();
+        bt_selectPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               selectPicture();
+            }
+        });
 
         //show the data if it's editing existing post
-        Intent i= this.getIntent();
+        Intent i = this.getIntent();
         postType = i.getStringExtra(PostHistoryActivity.POST_TYPE);
         Log.d("CREATE POST", postType);
-        if(postType.equals(PostHistoryActivity.EDIT_POST)){
+        if (postType.equals(PostHistoryActivity.EDIT_POST)) {
             //show all data;
             Post p = this.getIntent().getParcelableExtra(HistoryAdapter.POST_CONTENT);
             // TODO: 23/09/2017 push the info to the interface
@@ -85,7 +115,7 @@ public class CreatePost_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //create a post
-                Post newPost = new Post();
+               // Post newPost = new Post();
                 newPost.setAvailable(true);
                 newPost.setId(societyID);
 
@@ -141,7 +171,7 @@ public class CreatePost_Activity extends AppCompatActivity {
 
                 //upload the new post to the database
                 // TODO: 23/09/2017 create or update the post
-                switch (postType){
+                switch (postType) {
                     case PostHistoryActivity.EDIT_POST:
                         //update the post
                         break;
@@ -167,10 +197,35 @@ public class CreatePost_Activity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    private void selectPicture() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_INTENT);
+    }
 
-
-
+    @Override
+    protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_INTENT && resultCode ==RESULT_OK) {
+            Uri uri = data.getData();
+            Log.d("Uri", uri.toString());
+            final StorageReference filepath = mStorage.child("Post_Images").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    Log.d("downloadUrl", downloadUrl.toString());
+                   // bt_selectPicture.setImageURI(downloadUrl);
+                    Glide.with(CreatePost_Activity.this).load(downloadUrl).into(bt_selectPicture);
+                    // Glide.with(CreatePost_Activity.this).using(new FirebaseImageLoader()).load(filepath).into(bt_selectPicture);
+                    newPost.setImageUrl(downloadUrl.toString());
+                    Toast.makeText(CreatePost_Activity.this, "Upload Done.", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
     //ref.http://terryyamg.blogspot.com.au/2015/03/android-jump-datepicker-timepicker.html
